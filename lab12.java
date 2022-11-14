ef9dd3d0661f0bf3cdb76782063e943fb5f23184
Nikita <nruchkin.proto@gmail.com>
2022-11-14 13:21:43 +0300

    123

---
+++ lab12.java
@@ -0,0 +1,42 @@
+
+import java.io.*;
+
+public class lab12 {
+
+
+	public static void main(String[] args)
+	{
+
+		try {
+
+			FileReader fr = new FileReader("Input.txt");
+
+
+			FileWriter fw = new FileWriter("Output.txt");
+
+			String str = "";
+
+			int i;
+
+			while ((i = fr.read()) != -1) {
+
+				str += (char)i;
+			}
+
+			System.out.println(str);
+
+			fw.write(str);
+
+			fr.close();
+			fw.close();
+
+			System.out.println("File reading and writing both done");
+        }
+
+		catch (IOException e) {
+
+
+			System.out.println("There are some IOException");
+		}
+	}
+}
